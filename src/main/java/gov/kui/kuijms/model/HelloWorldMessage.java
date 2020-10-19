0725e4e6e553c23cc9538d210fdc571719a941cd
Serg N <serg_vn@inbox.ru>
2020-10-19 11:21:20 +0900

    создание HelloWorldMessage implements Serializable

---
+++ src/main/java/gov/kui/kuijms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package gov.kui.kuijms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 4196442524538964546L;
+
+    private UUID id;
+    private String message;
+}
