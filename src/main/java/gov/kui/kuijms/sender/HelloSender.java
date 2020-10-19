052fcc6e1fd99d1c57b78c689411ba883e37ee9d
Serg N <serg_vn@inbox.ru>
2020-10-19 17:00:39 +0900

    добавление  sendAndResiveMessage() в HelloSender

--- src/main/java/gov/kui/kuijms/sender/HelloSender.java
+++ src/main/java/gov/kui/kuijms/sender/HelloSender.java
@@ -56,26 +56,4 @@ public class HelloSender {
 
         System.out.println(receviedMsg.getBody(String.class)+"\n");
     }
-
-    @Scheduled(fixedRate = 2000)
-    public void sendAndResiveMessage() throws JMSException {
-
-        HelloWorldMessage message = HelloWorldMessage.builder()
-                .id(UUID.randomUUID())
-                .message("!!! Hello ")
-                .build();
-
-        Message receviedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RSV_QUEUE, session -> {
-            try {
-                Message helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
-                helloMessage.setStringProperty("_type","gov.kui.kuijms.model.HelloWorldMessage");
-                System.out.println("--- Отправка Hello ---");
-                return  helloMessage;
-            } catch (JsonProcessingException e) {
-                throw new JMSException("boom");
-            }
-        });
-
-        System.out.println(receviedMsg.getBody(String.class)+"\n");
-    }
 }
